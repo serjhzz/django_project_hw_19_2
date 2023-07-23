6c85ffb9ca121dd54f273676bf8ee893190f4af0
Sergey Sologub <sergey.sologub@rtlabs.ru>
2023-07-23 23:30:06 +0300

    add requirements, add models product and category

--- django_project_hw_19_2/settings.py
+++ django_project_hw_19_2/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'django_project_hw_19_2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catalog',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
     }
 }
 
