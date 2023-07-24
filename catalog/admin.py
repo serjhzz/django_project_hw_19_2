59fd4454601e0d7eeef3ca9797a8d31368b2c1ff
Sergey Sologub <sergey.sologub@rtlabs.ru>
2023-07-24 23:18:27 +0300

    add models fields

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
