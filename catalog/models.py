bffced6350b66fa3a3918670413c9da292eca57c
Sergey Sologub <sergey.sologub@rtlabs.ru>
2023-07-25 11:03:45 +0300

    add management

--- catalog/models.py
+++ catalog/models.py
@@ -15,6 +15,7 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+        ordering = ('name',)
 
 
 class Product(models.Model):
@@ -24,8 +25,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, **NULLABLE,
                                  verbose_name="Категория")
     price = models.IntegerField(**NULLABLE, verbose_name="Цена")
-    date_of_creation = models.DateTimeField()
-    date_of_last_modification = models.DateTimeField()
+    date_of_creation = models.DateTimeField(auto_now_add=True)
+    date_of_last_modification = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
