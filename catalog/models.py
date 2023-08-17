6020f2c33bc426dde358c674a5369b17f659e629
Sergey Sologub <sergey.sologub@rtlabs.ru>
2023-08-17 23:29:47 +0300

    cbv part 1

--- catalog/models.py
+++ catalog/models.py
@@ -35,3 +35,20 @@ class Product(models.Model):
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
         ordering = ('name',)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    slug = models.SlugField(unique=True, verbose_name="Slug")
+    content = models.TextField(verbose_name="Содержимое")
+    preview = models.ImageField(upload_to='blog_previews/', verbose_name="Превью", **NULLABLE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_published = models.BooleanField(default=False, verbose_name="Признак публикации")
+    view_count = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Запись блога"
+        verbose_name_plural = "Записи блога"
