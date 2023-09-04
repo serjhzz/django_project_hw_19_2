b26e24b260a39898ce49d68cf028d2e1c24181df
Sergey Sologub <sergey.sologub@rtlabs.ru>
2023-09-04 17:59:49 +0300

    hw22_2 part 1

---
+++ catalog/models/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    "Product",
+    "Category",
+    "Version",
+    "Blog",
+]
+
+from .product import Product
+from .category import Category
+from .version import Version
+from .blog import Blog
--- django_project_hw_19_2/settings.py
+++ django_project_hw_19_2/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'catalog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
